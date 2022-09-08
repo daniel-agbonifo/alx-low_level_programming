fce22fae3857762ff328f8707ff28f82983120b3
daniel-agbonifo <danielagbonifo2020@gmail.com>
2022-09-08 16:09:26 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * Description: main - Print out a string to stdout.
+ * Return: 0 if successfull.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
