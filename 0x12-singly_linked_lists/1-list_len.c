4cadabb48acdc613479427f5e188fb029888ae24
daniel-agbonifo <danielagbonifo2020@gmail.com>
2022-10-23 18:13:08 -0700

    linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,51 +1,26 @@
-i/*
-
-     * File: 1-list_len.c
-     *
-     *  * Auth: Brennan D Baraban
-     *
-     *   */
-
-
+/*
+ * File: 1-list_len.c
+ * Auth: Brennan D Baraban
+ */
 
 #include "lists.h"
 
-
-
 /**
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
  *
- *  * list_len - Finds the number of elements in
- *
- *   *            a linked list_t list.
- *
- *    * @h: The linked list_t list.
- *
- *     *
- *
- *      * Return: The number of elements in h.
- *
- *       */
-
+ * Return: The number of elements in h.
+ */
 size_t list_len(const list_t *h)
-
 {
+	size_t elements = 0;
 
-		size_t elements = 0;
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
 
-
-
-			while (h)
-
-					{
-
-								elements++;
-
-										h = h->next;
-
-											}
-
-
-
-				return (elements);
-
+	return (elements);
 }
